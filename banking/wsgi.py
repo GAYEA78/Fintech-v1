d5d80aca2cc3a9c0272cdcd02c3c8ad4b7bd937f
Arona Gaye <gayea@bc.edu>
2025-06-25 18:41:35 -0400

    Update wsgi.py

--- banking/wsgi.py
+++ banking/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'banking.settings')
 
 application = get_wsgi_application()
+
+if os.environ.get('RUN_MAIN') != 'true':  # Prevents double-run on reload
+    from django.core.management import call_command
+    try:
+        call_command('migrate')
+        call_command('collectstatic', interactive=False, verbosity=0)
+    except Exception as e:
+        print("Startup error:", e)
