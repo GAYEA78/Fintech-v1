d447e27efbeb8df464e6c4b4b0873e282af8aa3c
Arona Gaye <gayea@bc.edu>
2025-06-25 18:31:11 -0400

    Update settings.py

--- banking/settings.py
+++ banking/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import dotenv
+import dj_database_url
 dotenv.load_dotenv()
 load_dotenv()
 
@@ -66,12 +67,19 @@ WSGI_APPLICATION = 'banking.wsgi.application'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
