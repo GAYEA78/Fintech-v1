67eeb41f33ae023c0288ecc827d004967eda6602
Arona Gaye <gayea@bc.edu>
2025-06-25 18:42:58 -0400

    Update wsgi.py

--- banking/wsgi.py
+++ banking/wsgi.py
@@ -11,10 +11,14 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from django.contrib.auth import get_user_model #remove later
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'banking.settings')
 
 application = get_wsgi_application()
 
+
+#Remove below later
 if os.environ.get('RUN_MAIN') != 'true':  # Prevents double-run on reload
     from django.core.management import call_command
     try:
